faf953588ace477641e19ec76521255e57c1d8b1
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2021-05-12 12:35:41 +0200

    Cards of Arvala created

---
+++ The Mandalorian, Cards of Arvala/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    Touch touch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneChanger();
+    }
+
+    void SceneChanger()
+    {
+        if(Input.GetKey(KeyCode.E) /*|| touch.phase == TouchPhase.Began*/)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
