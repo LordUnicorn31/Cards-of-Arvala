334c0f7893bdec4b0df911cbb5713b83b0abe43a
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2021-05-19 12:37:35 +0200

    imagetarget funciona, ui added, menu destroyed

---
+++ The Mandalorian, Cards of Arvala/Assets/Scripts/Text.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Movement()
+    {
+        this.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
+    }
+}
