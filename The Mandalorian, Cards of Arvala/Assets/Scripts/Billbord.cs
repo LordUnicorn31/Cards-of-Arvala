1536f96e46d72d891630000f6c2fa7a9d472320e
bottzo <45202057+bottzo@users.noreply.github.com>
2021-05-21 11:18:18 +0200

    billboard script

---
+++ The Mandalorian, Cards of Arvala/Assets/Scripts/Billbord.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billbord : MonoBehaviour
+{
+    public Transform cam;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
