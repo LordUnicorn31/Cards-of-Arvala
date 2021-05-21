04fd62583d4d6418c6fbf2d22c3602f4c2800359
LordUnicorn31 <37652360+LordUnicorn31@users.noreply.github.com>
2021-05-21 12:31:05 +0200

    health functionality 2

---
+++ The Mandalorian, Cards of Arvala/Assets/Scripts/Jawa.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jawa : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    [HideInInspector] public int health;
+    [SerializeField] private int damage = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TakeDamage();
+    }
+
+    void TakeDamage()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            health -= damage;
+
+            if (health <= 0)
+            {
+                Debug.Log("Jawa Died!");
+            }
+        }
+    }
+}
