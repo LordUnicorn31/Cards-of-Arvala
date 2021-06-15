3716021cdd4e1de2be63c62a4aef9b1eafa7d862
LordUnicorn31 <37652360+LordUnicorn31@users.noreply.github.com>
2021-06-15 18:35:23 +0200

    Ui and main menu

--- The Mandalorian, Cards of Arvala/Assets/Scripts/Main.cs
+++ The Mandalorian, Cards of Arvala/Assets/Scripts/Main.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class Main : MonoBehaviour
 {
-    Touch touch;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +14,7 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SceneChanger();
-    }
-
-    void SceneChanger()
-    {
-        if(Input.GetKey(KeyCode.E) /*|| touch.phase == TouchPhase.Began*/)
+        if(Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene(1);
         }
